a66948d1b8e75bfa4bbd319e482b11f556c5885a
Catherin Cruz <cruz.catherin@gmail.com>
2022-06-05 20:20:25 -0500

    Updating cleaning host

--- bin/hello.js
+++ bin/hello.js
@@ -1,12 +1,12 @@
 const http = require('http');
  
-const host = '0.0.0.0';
+const host = '';
 const port = 8080;
  
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Time App - Hello!!!');
+  res.end('Time App - Hello buuuu!!!');
 });
  
 server.listen(port, host, () => {
