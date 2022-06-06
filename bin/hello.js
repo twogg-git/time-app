07ecefdbf080ff40cd392c16d3a094fa0e24698c
Catherin Cruz <cruz.catherin@gmail.com>
2022-06-05 21:07:33 -0500

    Updating host variable

--- bin/hello.js
+++ bin/hello.js
@@ -1,17 +1,12 @@
 const http = require('http');
  
 const host = '0.0.0.0';
-// var server = app.listen(PORT, function() {
-//   var host = server.address().address;
-//   var port = server.address().port;
-//   console.log("server is listening at http://%s:%s", host, port);
-// });
 const port = process.env.PORT || 80
  
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Time App - Hello buuuu!!!');
+  res.end('Time App - Hello yey!!!');
 });
  
 server.listen(port, host, () => {
