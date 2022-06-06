398dd18a90e3402fb79663ada3ec2397295aabd1
Cath Cruz <cruz.catherin@gmail.com>
2022-06-05 21:50:24 -0500

    Updating page up tp v1.1

--- bin/hello.js
+++ bin/hello.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 80
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Testing v1.0!!!');
+  res.end('Testing v1.1!!!');
 });
  
 server.listen(port, host, () => {
